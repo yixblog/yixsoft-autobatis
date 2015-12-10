d8f88a8e3438d69c556f3a0e4ff5afd331be63b8
yixblog <davepotter@163.com>
2015-12-10 17:45:24 +0800

    bug fix:select sql builder nullpointer

--- src/main/java/cn/yixblog/support/mybatis/autosql/core/providers/SelectSqlProvider.java
+++ src/main/java/cn/yixblog/support/mybatis/autosql/core/providers/SelectSqlProvider.java
@@ -2,9 +2,7 @@ package cn.yixblog.support.mybatis.autosql.core.providers;
 
 import cn.yixblog.support.mybatis.autosql.core.IAutoSqlProvider;
 
-import static org.apache.ibatis.jdbc.SqlBuilder.FROM;
-import static org.apache.ibatis.jdbc.SqlBuilder.SELECT;
-import static org.apache.ibatis.jdbc.SqlBuilder.SQL;
+import static org.apache.ibatis.jdbc.SqlBuilder.*;
 
 /**
  * auto select sql builder
@@ -14,6 +12,7 @@ public class SelectSqlProvider extends AbstractSqlProvider implements IAutoSqlPr
 
     @Override
     public String getSql() {
+        BEGIN();
         SELECT("*");
         FROM(getTableName());
         buildWhereClause();
