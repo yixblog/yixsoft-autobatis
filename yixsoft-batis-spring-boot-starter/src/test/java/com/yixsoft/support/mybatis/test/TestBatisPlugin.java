a887812b3c1f86e0fc374c83bb8620ba5d06c6ef
Yixian <davepotter@163.com>
2022-05-26 21:21:34 +0800

    1.0.14: fixtests

--- yixsoft-batis-spring-boot-starter/src/test/java/com/yixsoft/support/mybatis/test/TestBatisPlugin.java
+++ yixsoft-batis-spring-boot-starter/src/test/java/com/yixsoft/support/mybatis/test/TestBatisPlugin.java
@@ -6,13 +6,11 @@ import com.yixsoft.support.mybatis.test.mappers.BasicLogMapper;
 import com.yixsoft.support.mybatis.test.mappers.ExampleAutoEntityMapper;
 import com.yixsoft.support.mybatis.test.mappers.ExampleEntityMapper;
 import com.yixsoft.support.mybatis.test.mappers.Log2Mapper;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
@@ -24,10 +22,9 @@ import java.util.Map;
  * validate plugin
  * Created by yixian on 2015-08-31.
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
-public class TestBatisPlugin {
+class TestBatisPlugin {
     private BasicLogMapper logMapper;
     private Log2Mapper log2Mapper;
     private ExampleEntityMapper expMapper;
@@ -35,22 +32,22 @@ public class TestBatisPlugin {
 
     @Test
     @Rollback
-    public void validPlugin() {
+    void validPlugin() {
         Map<String, Object> itm = logMapper.findOne("aba");
-        Assert.assertNotNull("aba instance should be exists", itm);
+        Assertions.assertNotNull(itm, "aba instance should be exists");
         Map<String, Object> params = new HashMap<>();
         params.put("userid", "1");
         PageList<Map<String, Object>> list = logMapper.list(params, new PageBounds(1, 10));
-        Assert.assertFalse("userid=1 query result should not be empty", list.isEmpty());
+        Assertions.assertFalse(list.isEmpty(), "userid=1 query result should not be empty");
         Integer count = logMapper.count(params);
-        Assert.assertEquals("count result should be 2", 2L, (long) count);
+        Assertions.assertEquals(2L, (long) count, "count result should be 2");
 
         PageList<Map<String, Object>> pageList1 = logMapper.pageListOne(params, new PageBounds(1, 10));
-        Assert.assertEquals("query result should be equals", list.getPaginator().getTotalCount(), pageList1.getPaginator().getTotalCount());
+        Assertions.assertEquals(list.getPaginator().getTotalCount(), pageList1.getPaginator().getTotalCount(), "query result should be equals");
 
         PageList<Map<String, Object>> pageListFake = logMapper.pageListFake(params, new PageBounds(1, 10));
-        Assert.assertEquals("list size should be same", list.size(), pageListFake.size());
-        Assert.assertEquals("fake count sql returns 0", 0, pageListFake.getPaginator().getTotalCount());
+        Assertions.assertEquals(list.size(), pageListFake.size(), "list size should be same");
+        Assertions.assertEquals(0, pageListFake.getPaginator().getTotalCount(), "fake count sql returns 0");
 
         Map<String, Object> newLog = new HashMap<>();
         newLog.put("content", "111");
@@ -58,35 +55,35 @@ public class TestBatisPlugin {
         logMapper.save(newLog);
         String newLogId = newLog.get("id").toString();
 
-        Assert.assertNotNull("uuid generated", newLogId);
+        Assertions.assertNotNull(newLogId, "uuid generated");
         Map<String, Object> inserted = logMapper.findOne(newLogId);
-        Assert.assertNotNull("object should have been saved into db", inserted);
-        Assert.assertEquals("content should be fetched correctly", "111", inserted.get("content"));
+        Assertions.assertNotNull(inserted, "object should have been saved into db");
+        Assertions.assertEquals("111", inserted.get("content"), "content should be fetched correctly");
 
         Map<String, Object> demo = logMapper.findOneShort(newLogId);
-        Assert.assertNull("object should not exists when attach query params with advanceSelect", demo);
+        Assertions.assertNull(demo, "object should not exists when attach query params with advanceSelect");
 
         inserted.put("content", "222");
         inserted.put("id", Collections.singletonList(newLogId));
         logMapper.update(inserted);
         inserted = logMapper.findOne(newLogId);
-        Assert.assertEquals("update when id key is array should be accepted", "222", inserted.get("content"));
+        Assertions.assertEquals("222", inserted.get("content"),"update when id key is array should be accepted");
 
         logMapper.updateContentTo333(newLogId);
         inserted = logMapper.findOne(newLogId);
-        Assert.assertEquals("update when id key is array should be accepted", "333", inserted.get("content"));
+        Assertions.assertEquals("333", inserted.get("content"),"update when id key is array should be accepted");
 
 
         demo = logMapper.findOneShort(newLogId);
-        Assert.assertTrue("advance select should removed content column from result", demo != null && !demo.containsKey("content"));
+        Assertions.assertTrue(demo != null && !demo.containsKey("content"), "advance select should removed content column from result");
 
         logMapper.delete(newLogId);
-        Assert.assertNull("delete should work", logMapper.findOne(newLogId));
+        Assertions.assertNull(logMapper.findOne(newLogId), "delete should work");
 
         Map<String, Object> log2 = new HashMap<>();
         log2.put("name", "hhh");
         log2Mapper.save(log2);
-        Assert.assertNotNull("autoincrement id can get", log2.get("pkid"));
+        Assertions.assertNotNull(log2.get("pkid"), "autoincrement id can get");
 
         LogEntity log3 = new LogEntity().setUserid(123).setContent("bar");
         logMapper.saveEntity(log3);
@@ -96,32 +93,32 @@ public class TestBatisPlugin {
 
     @Test
     @Rollback
-    public void testScoreChange() {
+    void testScoreChange() {
         ExampleDAO dao = new ExampleDAO().setCreateTime(new Date()).setGroupName("foo");
         expMapper.save(dao);
-        Assert.assertNotNull(dao.getEntityId());
+        Assertions.assertNotNull(dao.getEntityId());
 
         ExampleDAO dao2 = expMapper.findOne(dao.getEntityId());
-        Assert.assertEquals("foo", dao2.getGroupName());
+        Assertions.assertEquals("foo", dao2.getGroupName());
 
         dao2.setGroupName("bar");
         expMapper.update(dao2);
         dao2 = expMapper.findOne(dao.getEntityId());
-        Assert.assertEquals("bar", dao2.getGroupName());
+        Assertions.assertEquals("bar", dao2.getGroupName());
 
         ExampleAnotherDAO another = new ExampleAnotherDAO().setTime(new Date()).setGname("etse");
         expMapper.save(another);
 
         ExampleDAO compare = expMapper.findOne(another.getEntityId());
-        Assert.assertEquals("etse", compare.getGroupName());
+        Assertions.assertEquals("etse", compare.getGroupName());
     }
 
     @Test
     @Rollback
-    public void testAutoIdWithCamelCase() {
+    void testAutoIdWithCamelCase() {
         ExampleAutoIdDAO dao = new ExampleAutoIdDAO().setCreateTime(new Date()).setGroupName("foo");
         expAutoIdMapper.save(dao);
-        Assert.assertNotNull(dao.getEntityId());
+        Assertions.assertNotNull(dao.getEntityId());
     }
 
     @Autowired
